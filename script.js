78bf894af562f0615248868f2b8dc0adc89f0c7b
Pawan <nepsonh@gmail.com>
2022-08-24 23:29:30 +0545

    initial function for changing turn done

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ console.log("Is it even working?");
 let music = new Audio("music.mp3");
 let turn = new Audio("ting.mp3");
 let gameover = new Audio("gameover.mp3");
+let turn = "X";
+//Function to change turn here
+const changeTurn = () => {
+  return turn === "X" ? "0" : "X";
+};
