44ff6e6cc4497807f4fc5e22f04a1f2b80f049c7
Pawan <nepsonh@gmail.com>
2022-08-24 23:47:10 +0545

    Array.from() understood

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Tic-Tac-Toe
+
 This will be tic tac toe game
+
+# Array.from() understood
+
+https://www.w3schools.com/jsref/jsref_from.asp
+https://www.javascripttutorial.net/es6/javascript-array-from/
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Is it even working?");
 let music = new Audio("music.mp3");
-let turn = new Audio("ting.mp3");
+let turn1 = new Audio("ting.mp3");
 let gameover = new Audio("gameover.mp3");
 let turn = "X";
 //Function to change turn here
@@ -11,3 +11,7 @@ const changeTurn = () => {
 const checkWin = () => {};
 
 //Logic for game
+
+let boxes = document.getElementsByClassName("box");
+console.log(Array.from("Pawan"));
+Array.from(boxes).forEach((element) => {});
