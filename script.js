e92b07aae2bfd672213e596c3631208b01572717
Pawan <nepsonh@gmail.com>
2022-08-28 21:45:39 +0545

    possibilities of winning

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ const changeTurn = () => {
   return turn === "X" ? "0" : "X";
 };
 //Function to check for a win
-const checkWin = () => {};
+const checkWin = () => {
+  let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2.4, 8],
+  ];
+};
 
 //Logic for game
 
